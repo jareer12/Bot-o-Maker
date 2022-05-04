cddabd62269ecfdc2cd75ec0ff4b1c5afc1e8f28
JUB0T <jforeverything2007@gmail.com>
2022-05-04 19:42:29 +0200

    Solved issue #1

--- database/bots.sql
+++ database/bots.sql
@@ -1,14 +1,10 @@
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
 CREATE TABLE IF NOT EXISTS `bots` (
   `name` text NOT NULL,
   `token` varchar(125) NOT NULL,
   `created` bigint(20) NOT NULL,
   `prefix` text NOT NULL,
   `client_id` text NOT NULL,
-  `id` int(11) NOT NULL,
+  `id` int(11) PRIMARY KEY AUTO_INCREMENT,
   `memes_feature` tinyint(1) NOT NULL DEFAULT 0,
   `poll_feature` tinyint(1) NOT NULL DEFAULT 0,
   `moderation_feature` tinyint(1) NOT NULL,
@@ -18,14 +14,7 @@ CREATE TABLE IF NOT EXISTS `bots` (
   `kick_feature` tinyint(1) NOT NULL,
   `ping_feature` tinyint(1) NOT NULL,
   `tag` text DEFAULT NULL,
-  `image_url` text DEFAULT NULL
+  `image_url` text DEFAULT NULL,
+  UNIQUE KEY `token` (`token`),
+  UNIQUE KEY `client_id` (`client_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-ALTER TABLE `bots`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `token` (`token`),
-  ADD UNIQUE KEY `client_id` (`client_id`) USING HASH;
-  
-ALTER TABLE `bots`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-COMMIT;
